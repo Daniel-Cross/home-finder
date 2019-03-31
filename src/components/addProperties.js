4bc3435d38cb0e0be82539b29c7d39c0eb2bf8fc
Daniel Cross <crossy1686@gmail.com>
2019-03-31 19:43:08 +0200

    form added

--- src/components/addProperties.js
+++ src/components/addProperties.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/addProperties.css';
 
-const AddProperty = () => {
-	return (
-		<div id="AddProperty">
-			<div>Add Property</div>
-		</div>
-	);
-};
+class AddProperties extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: '',
+			type: 'Apartment',
+			bedrooms: 0,
+			bathrooms: 0,
+			price: 0,
+			city: '',
+			email: '',
+		};
+	}
 
-export default AddProperty;
+	handleChange = (e) => {
+		const { name, value } = e.target;
+
+		this.setState({ [name]: value });
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	render() {
+		const { title, type, bedrooms, bathrooms, price, city, email } = this.state;
+		return (
+			<div id="AddProperties">
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="title">
+						Title:
+						<input type="text" onChange={this.handleChange} value={title} name="title" />
+					</label>
+					<label htmlFor="type">
+						Type:
+						<select value={type} onChange={this.handleChange} name="type">
+							<option value="apartment">Apartment</option>
+							<option value="house">House</option>
+							<option value="bungalow">Bungalow</option>
+							<option value="castle">Castle</option>
+						</select>
+					</label>
+					<label htmlFor="bedrooms">
+						Number of Bedrooms:
+						<input type="number" value={bedrooms} onChange={this.handleChange} name="bedrooms" />
+					</label>
+					<label htmlFor="bathrooms">
+						Bathrooms:
+						<input type="number" value={bathrooms} onChange={this.handleChange} name="bathrooms" />
+					</label>
+					<label htmlFor="price">
+						Price:
+						<input type="number" value={price} onChange={this.handleChange} name="price" />
+					</label>
+					<label htmlFor="city">
+						City:
+						<input type="text" value={city} onChange={this.handleChange} name="city" />
+					</label>
+					<label htmlFor="email">
+						Email:
+						<input type="email" value={email} onChange={this.handleChange} name="email" />
+					</label>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default AddProperties;
